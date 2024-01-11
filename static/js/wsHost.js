346be98c996cff4a15858f4f1d960b08e6bd1b52
justin-penner <justin.penner@inspired.consulting>
2024-01-11 11:14:53 +0100

    updating host for deployment

--- static/js/wsHost.js
+++ static/js/wsHost.js
@@ -1,6 +1,8 @@
 export function getHost(location) {
     if(location.includes('localhost')) {
         return 'ws://localhost:3000';
+    } else if(location.includes('87')) {
+        return 'wss://87.186.28.104:3000/';
     } else {
         return 'wss://spl-e931.onrender.com/';
     }
