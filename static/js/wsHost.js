b949de1f968ee69cce0fc11034d446207e51e1ab
Paul Strebinger <paul.strebinger@web.de>
2024-01-18 13:49:31 +0100

    Changed IP

--- static/js/wsHost.js
+++ static/js/wsHost.js
@@ -2,7 +2,7 @@ export function getHost(location) {
     if(location.includes('localhost')) {
         return 'ws://localhost:3000';
     } else if(location.includes('87')) {
-        return 'wss://87.186.28.104:3000/';
+        return 'wss://87.186.16.196:3000/';
     } else {
         return 'wss://spl-e931.onrender.com/';
     }
