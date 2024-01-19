34820202d780954fb9e53a9b191287469cfe73ee
VisualsOfPaul <username@users.noreply.github.com>
2024-01-19 12:52:44 +0100

    New test

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ const CLIENTS = [];
 APP.get("/connect", (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
+    // res.setHeader('Connection', 'keep-alive');
 
     CLIENTS.push(res);
 
