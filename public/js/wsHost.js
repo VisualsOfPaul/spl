5159633ece25b7d9aea8c3f0624ba59772f2124c
justinpennerthkoeln <justin_dan.penner@smail.th-koeln.de>
2024-01-20 19:32:03 +0100

    adding Start Poll function

--- public/js/wsHost.js
+++
@@ -1,9 +0,0 @@
-export function getHost(location) {
-    if(location.includes('localhost')) {
-        return 'ws://localhost:3000';
-    } else if(location.includes('87')) {
-        return 'ws://87.186.16.196:3000/';
-    } else {
-        return 'wss://spl-e931.onrender.com/';
-    }
-}
